1166fe2a2a27b233cdc0bebd97e9629fd885d9c0
Adam Tanai <adam_tanai@epam.com>
2026-03-01 21:38:26 +0100

    fix(database): add automatic db migration after publishing to azure

--- Backend/PhotoAlbum.Api/Program.cs
+++ Backend/PhotoAlbum.Api/Program.cs
@@ -38,6 +38,21 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
