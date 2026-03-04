71ce530370259f02067cb214de95a85726ff4f62
Adam Tanai <adam_tanai@epam.com>
2026-03-04 20:12:23 +0100

    Revert "fix(interceptor): clear token and navigate to login if 401 error"
    This reverts commit 3033d2da0d199569a60300c469cb423d24a741c2.

--- Frontend/src/app/auth/interceptors/token-interceptor.ts
+++ Frontend/src/app/auth/interceptors/token-interceptor.ts
@@ -1,28 +1,13 @@
-import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { catchError, throwError } from 'rxjs';
 import { SessionStorage } from '../services/session-storage';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
-  const sessionStorage = inject(SessionStorage);
-  const router = inject(Router);
-
-  const token = sessionStorage.getToken();
+  const token = inject(SessionStorage).getToken();
 
   if (token) {
     req = req.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
   }
 
-  return next(req).pipe(
-    catchError((error: HttpErrorResponse) => {
-      if (error.status === 401) {
-        sessionStorage.deleteToken();
-
-        router.navigate(['/login']);
-      }
-
-      return throwError(() => error);
-    }),
-  );
+  return next(req);
 };
